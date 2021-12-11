d09d6222cd4244c28e8fca37730916bf42778448
LauraMariaChiara <91607635+LauraMariaChiara@users.noreply.github.com>
2021-12-11 19:28:46 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+    int dim;
+    cin>>dim;
+    float numeri[dim];
+    for (int i=0; i<dim; i++){
+        cin >> numeri[i];
+    }
+    numeroMassimo(numeri, dim);
+    cout << numeroMassimo(numeri, dim) <<endl;
+    return 0;
 }
